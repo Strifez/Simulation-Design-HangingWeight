87868397c26aa7646d29fddd1d9e861d60ff1e6a
Jason <jason.huang11@hotmail.ca>
2017-02-12 16:39:50 -0500

    initial Commit

---
+++ Assets/Scripts/Mass.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Mass : MonoBehaviour {
+	public Text sliderValue;
+	public Slider mass;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	// Update is called once per frame
+	void Update () {
+		sliderValue.text = mass.value.ToString ();
+	}
+}
