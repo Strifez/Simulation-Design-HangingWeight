5b3cbec22742e5916d523bd9f6425eac1cd294b5
Jason <jason.huang11@hotmail.ca>
2017-02-12 18:42:10 -0500

    anchored the sliders

--- Assets/Scripts/OneTon.cs
+++ Assets/Scripts/OneTon.cs
@@ -12,9 +12,9 @@ public class OneTon : MonoBehaviour {
 	}
 
 	void OnMouseDrag(){
-		Vector3 cursorPoint = new Vector3 (Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
-		Vector3 cursorPosition = Camera.main.ScreenToWorldPoint (cursorPoint) + offset;
-		transform.position = cursorPosition;
+		Vector3 mousePoint = new Vector3 (Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+		Vector3 mousePosition = Camera.main.ScreenToWorldPoint (mousePoint) + offset;
+		transform.position = mousePosition;
 		transform.position = new Vector3 (0, transform.position.y, transform.position.z);
 	}
 	// Use this for initialization
@@ -23,7 +23,17 @@ public class OneTon : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+	{
+		/*
+		if (transform.position.y >= 7) {
+			Vector3 cursorPoint = new Vector3 (Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+			Vector3 cursorPosition = Camera.main.ScreenToWorldPoint (cursorPoint) + offset;
+			transform.position = cursorPosition;
+			transform.position = new Vector3 (0, -7, transform.position.z);
+		} else if (transform.position.y <= -1 && transform.position.y >= 7) {
+			OnMouseDrag ();
+		}
+		*/
 	}
 }
